6043794e15292fe1819ea41555e130e9a71b85a4
GNBarbaIsdefe <118733096+GNBarbaIsdefe@users.noreply.github.com>
2022-12-21 09:39:24 +0100

    New endpoint CNIG and logs

--- myApp.js
+++ myApp.js
@@ -54,7 +54,12 @@ app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
+  console.log(__dirname);
   response.sendFile(__dirname + '/views/index.html');
+});
+app.get("\CNIG", (request, response) => {
+  console.log('CNIG endpoint.');
+  response.send('Bienvenidos al despliegue atomático con Jenkins.')
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
